a5fa08af567472191fc679bfc473cf1a07fab902
Karambir Singh Nain <akarambir@gmail.com>
2013-03-23 22:03:27 +0530

    bootstrap theme changed

--- codesters/settings.py
+++ codesters/settings.py
@@ -129,7 +129,7 @@ MIDDLEWARE_CLASSES = (
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
     'django.core.context_processors.debug',
-    'django.core.context_processors.i18n',
+    #'django.core.context_processors.i18n',
     'django.core.context_processors.media',
     'django.core.context_processors.request',
     'django.core.context_processors.static',
