d883c1b936d8ddb52e003625c4999791cb4aeffb
Karambir Singh Nain <akarambir@gmail.com>
2013-04-02 18:28:51 +0530

    info pages added

--- codesters/urls.py
+++ codesters/urls.py
@@ -8,14 +8,17 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'codesters.views.home', name='home'),
-    # url(r'^codesters/', include('codesters.foo.urls')),
+    url(r'^$', HomeView.as_view(), name='page_home'),
+    url(r'^about/$', AboutView.as_view(), name='page_about'),
+    url(r'^team/$', TeamView.as_view(), name='page_team'),
+    url(r'^contact/$', ContactView.as_view(), name='page_contact'),
+    url(r'^guidelines/$', GuidelinesView.as_view(), name='page_guidelines'),
+    url(r'^accounts/profile/$', ProfileView.as_view(), name='user_profile'),
+)
 
-    url(r'^$', HomeView.as_view(), name='home'),
+urlpatterns += patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('registration.backends.default.urls')),
-    url(r'^accounts/profile/$', ProfileView.as_view(), name='user_profile'),
     url(r'^blog/', include('blogs.urls')),
     url(r'^feed/', include('feeds.urls')),
     url(r'^profile/', include('profiles.urls')),
--- codesters/views.py
+++ codesters/views.py
@@ -10,5 +10,17 @@ from django.contrib.auth.models import User
 class HomeView(TemplateView):
     template_name = 'index.html'
 
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+class TeamView(TemplateView):
+    template_name = 'team.html'
+
+class ContactView(TemplateView):
+    template_name = 'contact.html'
+
+class GuidelinesView(TemplateView):
+    template_name = 'guidelines.html'
+
 class ProfileView(LoginRequiredMixin, TemplateView):
     template_name = 'profile.html'
