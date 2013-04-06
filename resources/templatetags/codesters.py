3688daf1678001ecd502568c9948b7eef602d14b
Karambir Singh Nain <akarambir@gmail.com>
2013-04-06 20:06:59 +0530

    active tag merged in codesters

--- resources/templatetags/codesters.py
+++ resources/templatetags/codesters.py
@@ -6,6 +6,13 @@ from django import template
 
 register = template.Library()
 
+@register.simple_tag
+def active(request, pattern):
+    import re
+    if re.search(pattern, request.path):
+        return 'active'
+    return ''
+
 @register.filter
 def age(value):
     now = datetime.now()
