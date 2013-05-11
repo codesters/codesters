239157d25de15c2d923708e0e3e8a1b6e48168b2
Karambir Singh Nain <akarambir@gmail.com>
2013-05-11 08:18:35 +0530

    tracks urls and views removed, default django user profile url scheme redirect removed

--- codesters/views.py
+++ codesters/views.py
@@ -1,25 +1,10 @@
-from django.http import HttpResponseRedirect
-from django.template import RequestContext
-from django.shortcuts import render_to_response, get_object_or_404
-from django.core.urlresolvers import reverse
-from django.views.generic import TemplateView, ListView, RedirectView
-from guardian.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
 from braces.views import SetHeadlineMixin
-from django.contrib.auth.models import User
-
-def user_redirect_view(request, username):
-    user = get_object_or_404(User, username=username)
-    return HttpResponseRedirect(reverse('user_detail', kwargs={'username': user.username,}))
 
 
 class ExploreView(SetHeadlineMixin, TemplateView):
     template_name = 'coming_soon.html'
     headline = 'Explore - Coming Soon'
-
-
-class TrackHomeView(SetHeadlineMixin, TemplateView):
-    template_name = 'coming_soon.html'
-    headline = 'Tracks - Coming Soon'
 
 
 class HomeView(SetHeadlineMixin, TemplateView):
