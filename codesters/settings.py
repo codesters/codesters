b4107bbf51ff70d949112e691d028ec28550c1fc
Karambir Singh Nain <akarambir@gmail.com>
2013-05-11 17:23:21 +0530

    settings project root path reverted

--- codesters/settings.py
+++ codesters/settings.py
@@ -1,7 +1,6 @@
 import os
 
-ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-PROJECT_ROOT = os.path.join(ROOT, '..')
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
