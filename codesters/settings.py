cbd026e529fca0c62272889eceeb689658931cc1
Karambir Singh Nain <akarambir@gmail.com>
2013-05-11 17:17:23 +0530

    settings configuration reverted to main settings.py with local and prod

--- codesters/settings.py
+++ codesters/settings.py
@@ -155,3 +155,13 @@ LOGGING = {
         },
     }
 }
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
+
+try:
+    from prod_settings import *
+except ImportError:
+    pass
