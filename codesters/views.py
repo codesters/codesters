087f133511b014918db8865f1c368e0b97f29b0b
Karambir Singh Nain <akarambir@gmail.com>
2013-06-08 01:37:38 +0530

    resource detail now have table showing info, explore template too have tables for everything

--- codesters/views.py
+++ codesters/views.py
@@ -1,6 +1,6 @@
 from collections import Counter
 from urlparse import urlparse
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 from braces.views import SetHeadlineMixin
 from django.template import RequestContext
 from django.shortcuts import get_object_or_404, render_to_response
@@ -16,21 +16,37 @@ def popular_domains_mixin(number='more'):
         domains.append(urlparse(resource.url)[1])
     for domain in domains:
         cnt[domain] += 1
-    if number=='less': return cnt.most_common(5)
-    else: return cnt.most_common()
+    if number=='less':
+        return cnt.most_common(5)
+    else:
+        return cnt.most_common()
 
-def explore_all_domains(request):
-    ctx = {
-            'popular_domains': popular_domains_mixin()
-    }
-    return render_to_response('explore/explore_all_domains.html', ctx, context_instance=RequestContext(request))
 
-def explore_all_topics(request):
-    popular_topics = Topic.objects.annotate(no_of_resources=Count('resource')).order_by('-no_of_resources')
-    ctx = {
-            'popular_topics': popular_topics,
-    }
-    return render_to_response('explore/explore_all_topics.html', ctx, context_instance=RequestContext(request))
+class PopularTopicListView(SetHeadlineMixin, ListView):
+    context_object_name = 'popular_topics'
+    template_name = 'explore/explore_all_topics.html'
+    headline = 'Topics by Popularity'
+
+    def get_queryset(self):
+        return Topic.objects.annotate(no_of_resources=Count('resource')).order_by('-no_of_resources')
+
+
+class PopularDomainListView(SetHeadlineMixin, ListView):
+    context_object_name = 'popular_domains'
+    template_name = 'explore/explore_all_domains.html'
+    headline = 'All Domains by Popularity'
+
+    def get_queryset(self):
+        return popular_domains_mixin()
+
+
+class RecentResourceListView(SetHeadlineMixin, ListView):
+    context_object_name = 'resources'
+    template_name = 'explore/explore_all_recent_resources.html'
+    headline = 'Recent Resources'
+
+    def get_queryset(self):
+        return Resource.objects.all().order_by('-created_at')[:50]
 
 
 def explore_home(request):
