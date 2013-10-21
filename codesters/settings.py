c145cf0a1cad7317802f23c2867a67a429150875
Karambir Singh Nain <akarambir@gmail.com>
2013-10-21 15:16:34 +0530

    static 500 template, app templates moved

--- codesters/settings.py
+++ codesters/settings.py
@@ -106,7 +106,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
