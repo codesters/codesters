7e4aeee3bde05481e2e12788bca6fe83ea6788e9
Karambir Singh Nain <akarambir@gmail.com>
2013-10-23 20:36:20 +0530

    removed instance of snippet search class from search index

--- profiles/search_indexes.py
+++ profiles/search_indexes.py
@@ -1,19 +1,6 @@
 import datetime
 from haystack import indexes
-from profiles.models import Snippet, Project
-
-
-class SnippetIndex(indexes.SearchIndex, indexes.Indexable):
-    text = indexes.CharField(document=True, use_template=True)
-    user = indexes.CharField(model_attr='user')
-    content = indexes.CharField(model_attr='content')
-
-    def get_model(self):
-        return Snippet
-
-    def index_queryset(self, using=None):
-        return self.get_model().objects.filter(show=True).filter(updated_at__lte=datetime.datetime.now())
-
+from .models import Project
 
 
 class ProjectIndex(indexes.SearchIndex, indexes.Indexable):
